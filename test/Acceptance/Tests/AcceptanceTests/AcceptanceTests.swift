2645d6ee4cf7a64e409f2521a4d7605dd52e9d79
banky <banky@legend.xyz>
2025-01-10 14:43:01 -0500

    Remove unused

--- test/Acceptance/Tests/AcceptanceTests/AcceptanceTests.swift
+++ test/Acceptance/Tests/AcceptanceTests/AcceptanceTests.swift
@@ -1294,22 +1294,13 @@ final class AcceptanceTest: Sendable {
     let given: [Given]
     let when: When
     let expect: Expect
-    let only: Bool
-    let skip: Bool
 
     init(
-        given: [Given], when: When, expect: Expect, only: Bool = false,
-        skip: Bool = false
+        given: [Given], when: When, expect: Expect
     ) {
         self.given = given
         self.when = when
         self.expect = expect
-        self.only = only
-        self.skip = skip
-
-        if only, skip {
-            fatalError("Cannot set both `only` and `skip` for a test")
-        }
     }
 }
 
@@ -1963,7 +1954,6 @@ func buildResultToCalls(builderResult: QuarkBuilder.QuarkBuilderBase.BuilderResu
     #expect(address == EthAddress("0x103B7e61BBaa2F62028Ebf3Ea7C47dC74Bd3a617"))
 }
 
-// @Test("Acceptance Tests", arguments: filteredTests)
 func testAcceptanceTests(test: AcceptanceTest) async throws {
     let context = Context(sender: test.when.sender)
     for given in test.given {
