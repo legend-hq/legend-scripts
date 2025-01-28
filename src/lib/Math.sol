c077132193462fcbb9045331cba215f4b6d89e8f
kevincheng96 <kevincheng96@hotmail.com>
2025-01-28 09:17:25 -0800

    Remove unused Math functions

--- src/lib/Math.sol
+++ src/lib/Math.sol
@@ -9,15 +9,4 @@ library Math {
     function subtractFlooredAtOne(uint256 a, uint256 b) internal pure returns (uint256) {
         return a > b ? a - b : 1;
     }
-
-    function subtractFlooredAtZero(uint256 a, uint256 b, uint256 c) internal pure returns (uint256) {
-        if (a < b || a - b < c) {
-            return 0;
-        }
-        return a - b - c;
-    }
-
-    function max(uint256 a, uint256 b) internal pure returns (uint256) {
-        return a > b ? a : b;
-    }
 }
