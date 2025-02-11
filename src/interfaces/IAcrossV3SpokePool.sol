9efc76de435b287fd0fbf8512090536b927e3fba
Scott Silver <scott@legend.xyz>
2025-02-10 20:57:20 -0800

    include the remainder of V3SpokePoolInterface

--- src/interfaces/IAcrossV3SpokePool.sol
+++ src/interfaces/IAcrossV3SpokePool.sol
@@ -6,11 +6,9 @@ pragma solidity 0.8.27;
 
 // Contains structs and functions used by SpokePool contracts to facilitate universal settlement.
 interface IAcrossV3SpokePool {
-    /**
-     *
+    /**************************************
      *              ENUMS                 *
-     *
-     */
+     **************************************/
 
     // Fill status tracks on-chain state of deposit, uniquely identified by relayHash.
     enum FillStatus {
@@ -32,27 +30,25 @@ interface IAcrossV3SpokePool {
     // Slow fills are requested via requestSlowFill and executed by executeSlowRelayLeaf after a bundle containing
     // the slow fill is validated.
 
-    /**
-     *
+    /**************************************
      *              STRUCTS               *
-     *
-     */
+     **************************************/
 
     // This struct represents the data to fully specify a **unique** relay submitted on this chain.
     // This data is hashed with the chainId() and saved by the SpokePool to prevent collisions and protect against
     // replay attacks on other chains. If any portion of this data differs, the relay is considered to be
     // completely distinct.
     struct V3RelayData {
-        // The address that made the deposit on the origin chain.
-        address depositor;
-        // The recipient address on the destination chain.
-        address recipient;
+        // The bytes32 that made the deposit on the origin chain.
+        bytes32 depositor;
+        // The recipient bytes32 on the destination chain.
+        bytes32 recipient;
         // This is the exclusive relayer who can fill the deposit before the exclusivity deadline.
-        address exclusiveRelayer;
+        bytes32 exclusiveRelayer;
         // Token that is deposited on origin chain by depositor.
-        address inputToken;
+        bytes32 inputToken;
         // Token that is received on destination chain by recipient.
-        address outputToken;
+        bytes32 outputToken;
         // The amount of input token deposited by depositor.
         uint256 inputAmount;
         // The amount of output token to be received by recipient.
@@ -60,12 +56,29 @@ interface IAcrossV3SpokePool {
         // Origin chain id.
         uint256 originChainId;
         // The id uniquely identifying this deposit on the origin chain.
-        uint32 depositId;
+        uint256 depositId;
         // The timestamp on the destination chain after which this deposit can no longer be filled.
         uint32 fillDeadline;
         // The timestamp on the destination chain after which any relayer can fill the deposit.
         uint32 exclusivityDeadline;
         // Data that is forwarded to the recipient.
+        bytes message;
+    }
+
+    // Same as V3RelayData but using addresses instead of bytes32 & depositId is uint32.
+    // Will be deprecated in favor of V3RelayData in the future.
+    struct V3RelayDataLegacy {
+        address depositor;
+        address recipient;
+        address exclusiveRelayer;
+        address inputToken;
+        address outputToken;
+        uint256 inputAmount;
+        uint256 outputAmount;
+        uint256 originChainId;
+        uint32 depositId;
+        uint32 fillDeadline;
+        uint32 exclusivityDeadline;
         bytes message;
     }
 
@@ -84,26 +97,42 @@ interface IAcrossV3SpokePool {
         V3RelayData relay;
         bytes32 relayHash;
         uint256 updatedOutputAmount;
-        address updatedRecipient;
+        bytes32 updatedRecipient;
         bytes updatedMessage;
         uint256 repaymentChainId;
     }
 
-    // Packs together parameters emitted in FilledV3Relay because there are too many emitted otherwise.
+    // Packs together parameters emitted in FilledRelay because there are too many emitted otherwise.
     // Similar to V3RelayExecutionParams, these parameters are not used to uniquely identify the deposit being
     // filled so they don't have to be unpacked by all clients.
     struct V3RelayExecutionEventInfo {
-        address updatedRecipient;
-        bytes updatedMessage;
+        bytes32 updatedRecipient;
+        bytes32 updatedMessageHash;
         uint256 updatedOutputAmount;
         FillType fillType;
     }
 
-    /**
-     *
+    // Represents the parameters required for a V3 deposit operation in the SpokePool.
+    struct DepositV3Params {
+        bytes32 depositor;
+        bytes32 recipient;
+        bytes32 inputToken;
+        bytes32 outputToken;
+        uint256 inputAmount;
+        uint256 outputAmount;
+        uint256 destinationChainId;
+        bytes32 exclusiveRelayer;
+        uint256 depositId;
+        uint32 quoteTimestamp;
+        uint32 fillDeadline;
+        uint32 exclusivityParameter;
+        bytes message;
+    }
+
+    /**************************************
      *              EVENTS                *
-     *
-     */
+     **************************************/
+
     event FundsDeposited(
         bytes32 inputToken,
         bytes32 outputToken,
@@ -147,26 +176,47 @@ interface IAcrossV3SpokePool {
         V3RelayExecutionEventInfo relayExecutionInfo
     );
 
-    event RequestedV3SlowFill(
-        address inputToken,
-        address outputToken,
+    event RequestedSlowFill(
+        bytes32 inputToken,
+        bytes32 outputToken,
         uint256 inputAmount,
         uint256 outputAmount,
         uint256 indexed originChainId,
-        uint32 indexed depositId,
+        uint256 indexed depositId,
         uint32 fillDeadline,
         uint32 exclusivityDeadline,
-        address exclusiveRelayer,
-        address depositor,
-        address recipient,
-        bytes message
+        bytes32 exclusiveRelayer,
+        bytes32 depositor,
+        bytes32 recipient,
+        bytes32 messageHash
     );
 
-    /**
-     *
+    event ClaimedRelayerRefund(
+        bytes32 indexed l2TokenAddress,
+        bytes32 indexed refundAddress,
+        uint256 amount,
+        address indexed caller
+    );
+
+    /**************************************
      *              FUNCTIONS             *
-     *
-     */
+     **************************************/
+
+    function deposit(
+        bytes32 depositor,
+        bytes32 recipient,
+        bytes32 inputToken,
+        bytes32 outputToken,
+        uint256 inputAmount,
+        uint256 outputAmount,
+        uint256 destinationChainId,
+        bytes32 exclusiveRelayer,
+        uint32 quoteTimestamp,
+        uint32 fillDeadline,
+        uint32 exclusivityDeadline,
+        bytes calldata message
+    ) external payable;
+
     function depositV3(
         address depositor,
         address recipient,
@@ -178,6 +228,20 @@ interface IAcrossV3SpokePool {
         address exclusiveRelayer,
         uint32 quoteTimestamp,
         uint32 fillDeadline,
+        uint32 exclusivityDeadline,
+        bytes calldata message
+    ) external payable;
+
+    function depositNow(
+        bytes32 depositor,
+        bytes32 recipient,
+        bytes32 inputToken,
+        bytes32 outputToken,
+        uint256 inputAmount,
+        uint256 outputAmount,
+        uint256 destinationChainId,
+        bytes32 exclusiveRelayer,
+        uint32 fillDeadlineOffset,
         uint32 exclusivityDeadline,
         bytes calldata message
     ) external payable;
@@ -196,41 +260,76 @@ interface IAcrossV3SpokePool {
         bytes calldata message
     ) external payable;
 
+    function unsafeDeposit(
+        bytes32 depositor,
+        bytes32 recipient,
+        bytes32 inputToken,
+        bytes32 outputToken,
+        uint256 inputAmount,
+        uint256 outputAmount,
+        uint256 destinationChainId,
+        bytes32 exclusiveRelayer,
+        uint256 depositNonce,
+        uint32 quoteTimestamp,
+        uint32 fillDeadline,
+        uint32 exclusivityParameter,
+        bytes calldata message
+    ) external payable;
+
+    function speedUpDeposit(
+        bytes32 depositor,
+        uint256 depositId,
+        uint256 updatedOutputAmount,
+        bytes32 updatedRecipient,
+        bytes calldata updatedMessage,
+        bytes calldata depositorSignature
+    ) external;
+
     function speedUpV3Deposit(
         address depositor,
-        uint32 depositId,
+        uint256 depositId,
         uint256 updatedOutputAmount,
         address updatedRecipient,
         bytes calldata updatedMessage,
         bytes calldata depositorSignature
     ) external;
 
-    function fillV3Relay(V3RelayData calldata relayData, uint256 repaymentChainId) external;
-
-    function fillV3RelayWithUpdatedDeposit(
+    function fillRelay(
         V3RelayData calldata relayData,
         uint256 repaymentChainId,
+        bytes32 repaymentAddress
+    ) external;
+
+    function fillV3Relay(V3RelayDataLegacy calldata relayData, uint256 repaymentChainId) external;
+
+    function fillRelayWithUpdatedDeposit(
+        V3RelayData calldata relayData,
+        uint256 repaymentChainId,
+        bytes32 repaymentAddress,
         uint256 updatedOutputAmount,
-        address updatedRecipient,
+        bytes32 updatedRecipient,
         bytes calldata updatedMessage,
         bytes calldata depositorSignature
     ) external;
 
-    function requestV3SlowFill(V3RelayData calldata relayData) external;
+    function requestSlowFill(V3RelayData calldata relayData) external;
 
-    function executeV3SlowRelayLeaf(V3SlowFill calldata slowFillLeaf, uint32 rootBundleId, bytes32[] calldata proof)
-        external;
+    function executeSlowRelayLeaf(
+        V3SlowFill calldata slowFillLeaf,
+        uint32 rootBundleId,
+        bytes32[] calldata proof
+    ) external;
 
-    /**
-     *
+    function claimRelayerRefund(bytes32 l2TokenAddress, bytes32 refundAddress) external;
+
+    /**************************************
      *              ERRORS                *
-     *
-     */
+     **************************************/
+
     error DisabledRoute();
     error InvalidQuoteTimestamp();
     error InvalidFillDeadline();
     error InvalidExclusiveRelayer();
-    error InvalidExclusivityDeadline();
     error MsgValueDoesNotMatchInputAmount();
     error NotExclusiveRelayer();
     error NoSlowFillsInExclusivityWindow();
@@ -244,4 +343,77 @@ interface IAcrossV3SpokePool {
     error InvalidPayoutAdjustmentPct();
     error WrongERC7683OrderId();
     error LowLevelCallFailed(bytes data);
+    error InsufficientSpokePoolBalanceToExecuteLeaf();
+    error NoRelayerRefundToClaim();
+
+    /**************************************
+     *             LEGACY EVENTS          *
+     **************************************/
+
+    // Note: these events are unused, but included in the ABI for ease of migration.
+    event V3FundsDeposited(
+        address inputToken,
+        address outputToken,
+        uint256 inputAmount,
+        uint256 outputAmount,
+        uint256 indexed destinationChainId,
+        uint32 indexed depositId,
+        uint32 quoteTimestamp,
+        uint32 fillDeadline,
+        uint32 exclusivityDeadline,
+        address indexed depositor,
+        address recipient,
+        address exclusiveRelayer,
+        bytes message
+    );
+
+    event RequestedSpeedUpV3Deposit(
+        uint256 updatedOutputAmount,
+        uint32 indexed depositId,
+        address indexed depositor,
+        address updatedRecipient,
+        bytes updatedMessage,
+        bytes depositorSignature
+    );
+
+    // Legacy struct only used to preserve the FilledV3Relay event definition.
+    struct LegacyV3RelayExecutionEventInfo {
+        address updatedRecipient;
+        bytes updatedMessage;
+        uint256 updatedOutputAmount;
+        FillType fillType;
+    }
+
+    event FilledV3Relay(
+        address inputToken,
+        address outputToken,
+        uint256 inputAmount,
+        uint256 outputAmount,
+        uint256 repaymentChainId,
+        uint256 indexed originChainId,
+        uint32 indexed depositId,
+        uint32 fillDeadline,
+        uint32 exclusivityDeadline,
+        address exclusiveRelayer,
+        address indexed relayer,
+        address depositor,
+        address recipient,
+        bytes message,
+        LegacyV3RelayExecutionEventInfo relayExecutionInfo
+    );
+
+    event RequestedV3SlowFill(
+        address inputToken,
+        address outputToken,
+        uint256 inputAmount,
+        uint256 outputAmount,
+        uint256 indexed originChainId,
+        uint32 indexed depositId,
+        uint32 fillDeadline,
+        uint32 exclusivityDeadline,
+        address exclusiveRelayer,
+        address depositor,
+        address recipient,
+        bytes message
+    );
 }
