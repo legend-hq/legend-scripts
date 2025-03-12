0dee05c63953b50de53029693ec49a661f8aa5f0
Hans Wang <hans@legend.xyz>
2025-03-12 14:30:09 -0700

    aave scrtips initial before forge install

---
+++ src/AaveScripts.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: BSD-3-Clause
+pragma solidity 0.8.27;
+
+import {IERC20} from "openzeppelin/token/ERC20/IERC20.sol";
+import {SafeERC20} from "openzeppelin/token/ERC20/utils/SafeERC20.sol";
+import {DeFiScriptErrors} from "src/lib/DeFiScriptErrors.sol";
+
+contract AaveActions {
+
+}
